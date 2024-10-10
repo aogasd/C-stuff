5832e7e43deb73d58d8c75d6f019078f6157776a
aogasd <144658261+aogasd@users.noreply.github.com>
2024-10-10 13:16:04 +0300

    Initial commit

---
+++ hello world/helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+   char name[] = "Gareth the Dark";
+   char workplace[] = "The Banana Republic";
+   int years = 15;
+   char revenge1[] = "eating";
+   char revenge2[] = "spare change from the register";
+   printf("\n\nMy name is %s and I work for %s. I have been enslaved for %d years.\nI seek revenge by %s the %s.",name,workplace,years,revenge1,revenge2);
+   return 0;
+}
